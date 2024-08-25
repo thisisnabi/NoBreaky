8efad3a6470b704cc8f6c6f58af5b0f7cfcef7b5
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-25 23:57:18 +0330

    Add NoBreaky base class implementation

---
+++ src/NoBreaky/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace NoBreaky;
+
+internal static class Constants
+{
+    internal static class HttpMethod
+    {
+        internal const string GET = "get";
+        internal const string DELETE = "delete";
+        internal const string POST = "post";
+        internal const string PATCH = "patch";
+        internal const string PUT = "put";
+        internal const string HEAD = "head";
+        internal const string CONNECT = "connect";
+        internal const string OPTIONS = "options";
+        internal const string TRACE = "trace";
+    }
+}
--- src/NoBreaky/NoBreaky.cs
+++ src/NoBreaky/NoBreaky.cs
@@ -20,6 +20,31 @@ public class NoBreaky<TProgram> where TProgram : class
         return new NoBreaky<TProgram>(factory, baseOpenApiUrl);
     }
 
+    #region Http Verbs
+    public NoBreaky<TProgram> IsGetMethod() => Method(Constants.HttpMethod.GET);
+
+    public NoBreaky<TProgram> IsDeleteMethod() => Method(Constants.HttpMethod.DELETE);
+
+    public NoBreaky<TProgram> IsPostMethod() => Method(Constants.HttpMethod.POST);
+
+    public NoBreaky<TProgram> IsPatchMethod() => Method(Constants.HttpMethod.PATCH);
+
+    public NoBreaky<TProgram> IsPutMethod() => Method(Constants.HttpMethod.PUT);
+
+    public NoBreaky<TProgram> IsHeadMethod() => Method(Constants.HttpMethod.HEAD);
+
+    public NoBreaky<TProgram> IsConnectMethod() => Method(Constants.HttpMethod.CONNECT);
+
+    public NoBreaky<TProgram> IsOptionsMethod() => Method(Constants.HttpMethod.OPTIONS);
+
+    public NoBreaky<TProgram> IsTraceMethod() => Method(Constants.HttpMethod.TRACE);
+
+    public NoBreaky<TProgram> Method(string method)
+    { 
+        return this;
+    }
+    #endregion
+     
     public NoBreaky<TProgram> WithHeaders(Action<HeaderAssertionBuilder> headerAssertions)
     {
         var builder = new HeaderAssertionBuilder();
