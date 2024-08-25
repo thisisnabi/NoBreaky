b8405be4922356fd8d83dd035de2dfaa6de9185f
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-25 23:37:31 +0330

    Add Header assertion bases

---
+++ src/NoBreaky/Assertions/HeaderAssertion.cs
@@ -0,0 +1,8 @@
+﻿namespace NoBreaky.Assertions;
+
+public class HeaderAssertion
+{
+    public required string Name { get; set; }
+    public bool IsRequired { get; set; } = false;
+    public required Type Type { get; set; }
+}
